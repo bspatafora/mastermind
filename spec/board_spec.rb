ed05da722b2cf2d8ca4b4f10b7bb15155bdb7d9d
bspatafora <benjamin.spatafora@gmail.com>
2014-04-09 15:27:12 -0500

    Add Game class; remove Shoes dependency

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 module Mastermind
   describe Board do
 
-    it "is initialized with 10 Rows by default" do
+    it "is initialized with 10 rows by default" do
       board = Board.new
       expect(board.rows).to have(10).things
+    end
+
+    it "can be initialized with fewer rows" do
+      board = Board.new(1)
+      expect(board.rows).to have(1).things
     end
 
   end
