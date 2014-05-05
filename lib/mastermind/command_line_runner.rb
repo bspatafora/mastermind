7fdbde00c569a05c9afdf48db22b14da75a49b04
bspatafora <benjamin.spatafora@gmail.com>
2014-05-05 15:31:34 -0500

    Refactor CommandLineRunner

--- lib/mastermind/command_line_runner.rb
+++ lib/mastermind/command_line_runner.rb
@@ -11,12 +11,18 @@ module Mastermind
       @board.rows.each do |row|
         row.set_code_peg_holes(@computer.solicit_guess)
         @interface.draw_board
-        if @board.codebreaker_victory? || @board.codemaker_victory?
+        if game_over?
           @interface.say_game_over
           break
         end
         @interface.solicit_feedback(row)
       end
+    end
+
+    private
+
+    def game_over?
+      @board.codebreaker_victory? || @board.codemaker_victory?
     end
   end
 end
--- spec/command_line_runner_spec.rb
+++ spec/command_line_runner_spec.rb
@@ -3,15 +3,16 @@ require 'spec_helper'
 describe Mastermind::CommandLineRunner do
   before do
     @board = Mastermind::Board.new
-    @interface = Mastermind::CommandLineInterface.new(@board)
-    @computer = Mastermind::Computer.new(@board)
+    @interface = double("Command line interface",
+                        :solicit_code => "1111",
+                        :solicit_feedback => "",
+                        :draw_board => nil)
+    @computer = double("Computer", :solicit_guess => [1, 1, 2, 2])
     @game = Mastermind::CommandLineRunner.new(@interface, @board, @computer)
   end
 
   describe '#play' do
     it "runs until the game is over" do
-      @interface.stub(:gets) { "1111" }
-      @computer.stub(:solicit_guess) { [1, 1, 2, 2] }
       expect(@interface).to receive(:say_game_over)
       @game.play
     end
